cf9b34e7b835c8a3bcf41a71d97c0c25eee116cc
Alan Castilleja <alan.castilleja.94@gmail.com>
2021-04-26 14:50:02 -0500

    historical functions

--- practiceData.py
+++ practiceData.py
@@ -10,46 +10,67 @@ import psycopg2
 
 print('Layer 1s are coins like BTC or LTC while Tokens are on L2s like ERC20s/ERC721s')
 choice = str(input('choose which one to look up: coin or token? '))
+choice2 = str(input('Latest price or historical?: '))
 
 
 
 
 if choice.lower() == 'token':
+    if choice2.lower() == 'latest':
 
-    tokenAddress = str(input('What token do you want to look up? '))
+        tokenAddress = str(input('input hash of token do you want to look up? '))
 
-    url = "https://web3api.io/api/v2/market/tokens/prices/" + tokenAddress + '/latest'
+        url = "https://web3api.io/api/v2/market/tokens/prices/" + tokenAddress + '/latest'
 
-    headers = {"x-api-key": secrets.api_key}
+        headers = {"x-api-key": secrets.api_key}
 
-    response = requests.get(url, headers=headers)
-    query = json.loads(response.content)
-    payload = query['payload']
-    tkADD = payload[0]['address']
-    tkNAME = payload[0]['name']
-    tkSYMBOL = payload[0]['symbol']
-    tkPRICE = payload[0]['priceUSD']
-    print(tkADD, tkPRICE, tkNAME, tkSYMBOL)
+        response = requests.get(url, headers=headers)
+        query = json.loads(response.content)
+        payload = query['payload']
+        tkADD = payload[0]['address']
+        tkNAME = payload[0]['name']
+        tkSYMBOL = payload[0]['symbol']
+        tkPRICE = payload[0]['priceUSD']
+        print(tkADD, tkPRICE, tkNAME, tkSYMBOL)
 
-    db_string = 'postgresql://postgres:'+secrets.password+'@localhost/practiceDataDB'
-    db = models.create_engine(db_string, echo=False)
-    base = models.declarative_base()
-    base.metadata.create_all(db)
-    db_Session = sessionmaker(db)
-    db_session = db_Session()
-    result = Price(
-        address=tkADD,
-        name=tkNAME,
-        symbol=tkSYMBOL,
-        price=tkPRICE
-    )
+        db_string = 'postgresql://postgres:'+secrets.password+'@localhost/practiceDataDB'
+        db = models.create_engine(db_string, echo=False)
+        base = models.declarative_base()
+        base.metadata.create_all(db)
+        db_Session = sessionmaker(db)
+        db_session = db_Session()
+        result = Price(
+            address=tkADD,
+            name=tkNAME,
+            symbol=tkSYMBOL,
+            price=tkPRICE
+        )
 
-    db_session.add(result)
-    db_session.commit()
-    print('entry added')
-    db_session.close()
+        db_session.add(result)
+        db_session.commit()
+        print('entry added')
+        db_session.close()
+    if choice2.lower() == 'historical':
+        # thinking of calling latest api to just get name to attach to response
+        # https://docs.amberdata.io/reference#get-historical-token-price for questions on querystring
+        # need to ask Brian several architecture questions and if each token gets their own database
+        tokenAddress2 = str(input('input hash of token you want to look up? '))
+        url = "https://web3api.io/api/v2/market/tokens/prices/"+tokenAddress2+"/historical"
 
-    #print(json.loads(response.content))
+        querystring = {"timeInterval": "d", "timeFormat": "iso"}
+
+        headers = {"x-api-key": secrets.api_key}
+
+        response = requests.get(url, headers=headers, params=querystring)
+        query2 = json.loads(response.content)
+        # historical data api doesn't give a name in the output field
+        payload2 = query2['payload']['data']
+        for p in payload2:
+            print(p[0], p[1])
+        # print(json.dumps(payload2, indent=4, sort_keys=True))
+
+        #print(response.text)
+
 
 
 # ask Brian about api cost
